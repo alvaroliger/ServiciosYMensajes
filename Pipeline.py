d50020fce47ee884c451019a68ff4eba71aa11e0
Álvaro Liger Cámara <152375382+alvaroliger@users.noreply.github.com>
2026-02-11 18:07:42 +0100

    Add files via upload

---
+++ Pipeline.py
@@ -0,0 +1,24 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                sh 'python3 -m venv venv'
+                sh '. venv/bin/activate && pip install -r requirements.txt'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                sh 'python3 -m pytest'
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                sh 'echo "Despliegue simulado"'
+            }
+        }
+    }
+}
